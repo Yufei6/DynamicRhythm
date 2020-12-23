da96ac1fe70e6d4c25e1deef935473ddefb29fb1
Yufei <32465277+Yufei6@users.noreply.github.com>
2020-12-24 05:10:33 +0800

    ameliorer classes Instruction & Song

--- Assets/Scripts/Classes/Instruction.cs
+++ Assets/Scripts/Classes/Instruction.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 
 public class Instruction
 {
-    public Instruction(){
-        
+    const int ONEACTIONTOP = 0;
+    const int ONEACTIONSIDE = 1;
+    const int TWOACTIONTOP = 2;
+    const int TWOACTIONSIDE = 3;
+    const int ONEACTIONTOPONEACTIONSIDE = 4;
+
+    // enum Actions
+    // {
+    //     ONEACTIONTOP,
+    //     ONEACTIONSIDE,
+    //     TWOACTIONTOP,
+    //     TWOACTIONSIDE,
+    //     ONETOPONESIDE
+    // }
+
+    private int action;
+    private float timing;
+
+    public Instruction(int a, float t)
+    {
+        action = a;
+        timing = t;
+    }
+
+    public int getAction()
+    {
+        return action;
+    }
+
+    public float getTime()
+    {
+        return timing;
     }
 }
--- Assets/Scripts/Classes/Song.cs
+++ Assets/Scripts/Classes/Song.cs
@@ -1,11 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
 public class Song 
 {
-    public Song()
-    {
+    static int idKey=0;
+    private int id;
+    private string name;
+    private ArrayList listIns;
 
+    public Song(string name)
+    {
+        id = idKey;
+        idKey += 1;
+        listIns = new ArrayList();
     }
+
+    public void AddIns(Instruction i)
+    {
+        listIns.Add(i);
+    }
+
+    public void ReadFile(string filepath)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines(filepath);
+            foreach (string l in lines)
+            {
+                string[] words = l.Split(',');
+                try
+                {
+                    Instruction ins = new Instruction(Convert.ToInt32(words[1]),Convert.ToSingle(words[0]));
+                    listIns.Add(ins);
+                }
+                catch (System.Exception)
+                {
+                    Debug.Log("An exception when change String to int/float has been thrown!");
+                }
+                
+            }
+        }
+        catch(IOException)
+        {
+            Debug.Log("An IO exception has been thrown!");
+        }
+    }
+
+    public ArrayList getListIns()
+    {
+        return listIns;
+    }
+
+
 }
+
