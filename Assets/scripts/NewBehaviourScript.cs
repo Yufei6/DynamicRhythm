bfa6704100ca24a282b5985bf9bcb5e5f4d401b1
xiewf2019 <714253971@qq.com>
2020-12-23 21:56:34 +0100

    finish update

--- Assets/scripts/NewBehaviourScript.cs
+++ Assets/scripts/NewBehaviourScript.cs
@@ -4,32 +4,53 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-	private Transform RS_bone,LS_bone;
-	private Animator anim;
+	private Transform RS_bone,LS_bone,LS1_bone;
+	public Animator anim;
    private Quaternion rciq,lciq,riq,liq;
    const float MOVE_PER_CLOCK = 0.01f;
+   private int i=0;
     // Start is called before the first frame update
     void Start()
     {
-       anim = (Animator)FindObjectOfType (typeof(Animator)); 
+
        RS_bone = anim.GetBoneTransform (HumanBodyBones.RightUpperArm);
        LS_bone = anim.GetBoneTransform (HumanBodyBones.LeftUpperArm);
+       LS1_bone = anim.GetBoneTransform (HumanBodyBones.LeftUpperLeg);
+
        riq = RS_bone.rotation;
-       liq = LS_bone.rotation;
+       liq = LS1_bone.rotation;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Quaternion lb = LS_bone.transform.rotation * Quaternion.Inverse(liq);
-        lb.x += 1 * MOVE_PER_CLOCK;
-        lb.y += 1* MOVE_PER_CLOCK;
-        lb.z += 1 * MOVE_PER_CLOCK;
-        LS_bone.rotation = lb * liq;
+        if(i<6){
+          Quaternion lb = LS1_bone.transform.rotation * Quaternion.identity;
+          //lb.x += 3 * MOVE_PER_CLOCK;
+          //lb.y += 3* MOVE_PER_CLOCK;
+          lb.z += 3 * MOVE_PER_CLOCK;
+          LS1_bone.rotation = lb * liq;
+          i++;
+        }
+        if(i>=6){
+          i++;
+
+        }
+        if (i>=50)
+        {
+          anim.enabled= true;
+        }
+        if(i>=100){
+          anim.enabled= false;
+          i=0;
+        }
+
         Quaternion rb = RS_bone.transform.rotation * Quaternion.Inverse(riq);
-        rb.x += 1 * MOVE_PER_CLOCK;
-        rb.y += 1 * MOVE_PER_CLOCK;
-        rb.z += 1 * MOVE_PER_CLOCK;
+
+        //rb.x += 3* MOVE_PER_CLOCK;
+        //rb.y += 3 * MOVE_PER_CLOCK;
+        rb.z += 10 * MOVE_PER_CLOCK;
         RS_bone.rotation = rb * riq;
     }
 }
