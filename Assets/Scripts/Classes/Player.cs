974f1ca99669cf0dd0f80e61232ab87b9b7993e8
xiewf2019 <714253971@qq.com>
2020-12-25 14:17:36 +0100

    Update Player.cs

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Player 
 {
+	private string name;
+	private float vdi;
+	private int limit;
+	private Score score;
+	private Feature feature;
+	private float capD;
+	private float capG;
+	private string filename;
     public Player()
     {
 
     }
+    public void load(){
+
+    }
+    public void save(){
+    	
+    }
+    
 }
