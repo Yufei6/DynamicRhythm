567d6b43c64229f2ba07c3128a1adce8bd75e4fd
xiewf2019 <714253971@qq.com>
2021-01-03 00:05:30 +0100

    calcul

---
+++ Assets/scripts/playscene/calculAdaption.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class calculAdaption : MonoBehaviour
+{
+	public Player player;
+	public int NbTotal;
+	public float NbGauche;
+	public float NbDroite;
+    // Start is called before the first frame update
+    void Start()
+    {
+     // NbTotal=Math.floor(player.vdi * listIns.Count/100);
+     // NbGauche= player.capG * NbTotal/100;
+     // NbDroite= NbTotal - NbGauche;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
