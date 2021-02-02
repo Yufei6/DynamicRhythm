f046aa0007042f663fead039cd1a46486bf3a735
xiewf2019 <714253971@qq.com>
2021-02-02 15:58:02 +0100

    up

---
+++ Assets/Scenes/playscene/Joyconreader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joyconreader : MonoBehaviour
+{
+	private List<Joycon>    m_joycons;
+	private Joycon          m_joyconL;
+    private Joycon          m_joyconR;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	m_joycons = JoyconManager.Instance.j;
+
+        if ( m_joycons == null || m_joycons.Count <= 0 ) return;
+
+        m_joyconL = m_joycons.Find( c =>  c.isLeft );
+        m_joyconR = m_joycons.Find( c => !c.isLeft );
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var accel = m_joyconL.GetAccel();
+        var accel1 = m_joyconR.GetAccel();
+        Debug.Log("L"+accel.x);
+        Debug.Log("R"+accel1.x);
+    }
+}
