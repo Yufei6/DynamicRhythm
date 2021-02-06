b4f73f62c9d51a1e80c461246fa3abecad5132cc
Yufei <32465277+Yufei6@users.noreply.github.com>
2021-02-06 22:19:30 +0800

    update

--- Assets/Scripts/Classes/Controller.cs
+++ Assets/Scripts/Classes/Controller.cs
@@ -81,65 +81,95 @@ public class Controller
         managerTrace.Save(pathTrace);
     }
 
-    public Trace AnalyseFromSmallTrace(string filepath){
+    public string SearchFileName(string folder_path)
+    {
+        DirectoryInfo folder = new DirectoryInfo(folder_path);
+        int count_file = 0;
+        string file_name = "";
+            
+        foreach (FileInfo file in folder.GetFiles("*.txt"))
+        {
+            file_name = file.FullName;
+            count_file += 1 ;        
+        }
+        if (count_file == 1){
+            return file_name;
+        }
+        else{
+            return "Error";
+        }
+    }
+
+    public Trace AnalyseFromSmallTrace(){
         int idSong = 0;
         string userName;
         string datetime = "";
         int framePerSecond;
-        List<List<float>> list_acc = new List<List<float>>();
-        try
+
+        //Search new small trace file
+        string folder_path = "Assets/Trace/New";
+        string file_path = SearchFileName(folder_path);
+        if (file_path != "Error")
         {
-            string[] lines = File.ReadAllLines(filepath);
-            int i = 0;
-            foreach (string l in lines)
+
+            List<List<float>> list_acc = new List<List<float>>();
+            try
             {
-                string[] words = l.Split(',');
-                //the first line of file smallTrace
-                if (i==0){
-                    try
-                    {
-                        idSong = Convert.ToInt32(words[0]);
-                        userName = words[1];
-                        datetime = words[2];
-                        framePerSecond = Convert.ToInt32(words[3]);
+                string[] lines = File.ReadAllLines(file_path);
+                int i = 0;
+                bool meet_star = false;
+                foreach (string l in lines)
+                {
+
+                    string[] words = l.Split(',');
+                    //the first line of file smallTrace
+                    if (i==0){
+                        try
+                        {
+                            idSong = Convert.ToInt32(words[0]);
+                            userName = words[1];
+                            datetime = words[2];
+                            framePerSecond = Convert.ToInt32(words[3]);
+                        }
+                        catch(System.Exception)
+                        {
+                            Debug.Log("An exception when change String to int has been thrown!");
+                        }
                     }
-                    catch(System.Exception)
-                    {
-                        Debug.Log("An exception when change String to int has been thrown!");
+                    else{
+                        try
+                        {
+                            float acc_x_left = Convert.ToSingle(words[0]);
+                            float acc_y_left = Convert.ToSingle(words[1]);
+                            float acc_z_left = Convert.ToSingle(words[2]);
+                            float acc_x_right = Convert.ToSingle(words[3]);
+                            float acc_y_right = Convert.ToSingle(words[4]);
+                            float acc_z_right = Convert.ToSingle(words[5]);
+                            List<float> item = new List<float>(new float[]{acc_x_left, acc_y_left, acc_z_left, acc_x_right, acc_y_right, acc_z_right});
+                            list_acc.Add(item);
+                        }
+                        catch (System.Exception)
+                        {
+                            Debug.Log("An exception when change String to int has been thrown!");
+                        }
                     }
+                    i = i + 1;
                 }
-                else{
-                    try
-                    {
-                        float acc_x_left = Convert.ToSingle(words[0]);
-                        float acc_y_left = Convert.ToSingle(words[1]);
-                        float acc_z_left = Convert.ToSingle(words[2]);
-                        float acc_x_right = Convert.ToSingle(words[3]);
-                        float acc_y_right = Convert.ToSingle(words[4]);
-                        float acc_z_right = Convert.ToSingle(words[5]);
-                        List<float> item = new List<float>(new float[]{acc_x_left, acc_y_left, acc_z_left, acc_x_right, acc_y_right, acc_z_right});
-                        list_acc.Add(item);
-                    }
-                    catch (System.Exception)
-                    {
-                        Debug.Log("An exception when change String to int has been thrown!");
-                    }
-                }
-                i = i + 1;
             }
+            catch(IOException)
+            {
+                Debug.Log("An IO exception has been thrown!");
+            }
+
+            Song s = bibioSong.GetSong(idSong);
+            ArrayList ins = s.GetListIns();
+
+            //Compare the trace and ins
+
+            Trace t = new Trace(0, 0, 0, 0, 0, 0, s.name, 0, 0, 0, 0,0, datetime);
+            return t;
         }
-        catch(IOException)
-        {
-            Debug.Log("An IO exception has been thrown!");
-        }
 
-        Song s = bibioSong.GetSong(idSong);
-        ArrayList ins = s.GetListIns();
-
-        //Compare the trace and ins
-
-
-        Trace t = new Trace(0, 0, 0, 0, 0, 0, s.name, 0, 0, 0, 0,0, datetime);
-        return t;
+        return null;
     }
 }
