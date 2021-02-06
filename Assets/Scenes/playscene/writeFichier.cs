f61512d81d1d2794afe771c884fe49f1ebbebdc8
xiewf2019 <714253971@qq.com>
2021-02-06 12:50:45 +0100

    Update writeFichier.cs

--- Assets/Scenes/playscene/writeFichier.cs
+++ Assets/Scenes/playscene/writeFichier.cs
@@ -22,20 +22,21 @@ public class writeFichier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        write("a","b","c");
+        //write("a","b","c");
     }
-    void write(ArrayList actionframe,ArrayList acclL,ArrayList acclR,)
+    void write(ArrayList actionframe,ArrayList acclL,ArrayList acclR)
     {
-    	hour = System.DateTime.Now.Hour;
-        minute = System.DateTime.Now.Minute;
-        second = System.DateTime.Now.Second;
-        year = System.DateTime.Now.Year;
-        month = System.DateTime.Now.Month;
-        day = System.DateTime.Now.Day;
-        currentTimeText = string.Format("{0:D2}:{1:D2}:{2:D2} " + "{3:D4}/{4:D2}/{5:D2}", hour, minute, second, year, month, day);
-    	string filename=player.name+"_"+currentTimeText+"_"+songname+"_trace.txt"
+    	var hour = System.DateTime.Now.Hour;
+        var minute = System.DateTime.Now.Minute;
+        var second = System.DateTime.Now.Second;
+        var year = System.DateTime.Now.Year;
+        var month = System.DateTime.Now.Month;
+        var day = System.DateTime.Now.Day;
+        string currentTimeText = string.Format("{0:D2}:{1:D2}:{2:D2} " + "{3:D4}/{4:D2}/{5:D2}", hour, minute, second, year, month, day);
+    	string songname="sss";
+    	string filename=player.name+"_"+currentTimeText+"_"+songname+"_trace.txt";
     	string path="Assets/scripts/"+filename;
-    	string firstline="aaa"；
+    	string firstline="aaa";
     	FileStream fs = new FileStream("Assets/scripts/test1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite); //可以指定盘符，也可以指定任意文件名，还可以为word等文件
         StreamWriter sw = new StreamWriter(fs); // 创建写入流
         sw.WriteLine("bob hu");
